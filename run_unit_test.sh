fddee5150f510601860ee7045dc060f212f6254e
amitpoorab <amitpoorab@gmail.com>
2019-08-03 10:17:24 -0400

    Add files via upload

---
+++ run_unit_test.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+if [ "$REAL_DATABASE" = "" ]; then
+	until (nc -z -v -w30 db 3306 2> /dev/null)
+	do
+	  echo "Waiting for database connection..."
+	  sleep 5
+	done
+else
+	echo "Skipping db wait"
+fi
+/usr/local/spark-2.4.3-bin-hadoop2.7/bin/spark-submit --jars jars/mysql-connector-java-5.1.48-bin.jar src/main/python/test/unit-tests.py --numofcores 6
